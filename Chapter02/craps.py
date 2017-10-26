4a5525e0e800f7109676f7d477e82768a1fe3a0a
pbell20 <pbell20@hanoverstudents.org>
2017-10-26 11:14:59 -0400

    Third runthrough with added logic

--- Chapter02/craps.py
+++ Chapter02/craps.py
@@ -14,31 +14,73 @@
 # by Mr. Ciccolo
 
 import random
-
-
+totalwinnings = 100
+bet = 0
 def main():
+    global totalwinnings
+    global bet
     display_welcome()
     play_again = True
-    while play_again:
+    while play_again == True:
+        playerbet()
         total = roll_dice()
         if total == 7 or total == 11:
             print("You win!")
+            totalwinnings = totalwinnings + bet
+            print("Your current balance is $", totalwinnings)
         elif total == 2 or total == 3 or total == 12:
             print("You lose!")
+            totalwinnings = totalwinnings - bet
+            print("Your current balance is $", totalwinnings)
         else:
             re_roll(total)
-
         print() # Blank line for spacing
-        play_again = (input("Enter Y to play another round ") == 'Y')
-        clear_screen()
+        if totalwinnings > 5:
+            play_again_n = "x"
+            while play_again_n == "x":
+                play_again_n = (input("Press enter to play another round or n to keep your money"))
+                if play_again_n == "n" :
+                    print("Congrats, you left the game without losing it all. You left with $", totalwinnings)
+                    play_again = False
+                if play_again_n == "":
+                    play_again = True
+                else:
+                    play_again_n = "x"
+            clear_screen()
+        if totalwinnings == 0:
+            print("Ya broke, get out!")
+            play_again = False
+        elif totalwinnings < 5:
+            print("Ya done, you can't be here with only $", totalwinnings)
+            play_again = False
 
-
+def playerbet():
+    global bet
+    global totalwinnings
+    bet = 0
+    while bet == 0:
+        bet = eval(input("You have $%s How much would you like to bet" % totalwinnings))
+        if bet == "":
+            print("Please place a bet")
+            print("")
+        if bet >= 5 and bet <= totalwinnings:
+            print("")
+        elif bet < 5:
+            print("Don't be weak, you have to bet at least $5")
+            bet = 0
+            print("")
+        else:
+            print("I aint stupid, you don't have $", bet, "to bet!")
+            bet = 0
+            print("")
+    return bet
 def clear_screen():
     for i in range(20):
         print()
 
 
 def display_welcome():
+    global totalwinnings
     print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
     print("$                                              $")
     print("$ Welcome to the Computer Science Craps Table! $")
@@ -47,7 +89,8 @@ def display_welcome():
     print()
 
 def roll_dice():
-    input("Press Enter to roll the dice...") # We don't do anything with the input, we're just using it to pause the game
+    global bet
+    input("Press Enter to roll the dice... $%s is on the line" %bet) # We don't do anything with the input, we're just using it to pause the game
 
     die1 = random.randrange(1, 7)
     die2 = random.randrange(1, 7)
@@ -65,15 +108,20 @@ def roll_dice():
 def re_roll(point):
     print("You have to keep rolling until you get another", point)
     print() # Blank line for spacing
-
+    global totalwinnings
+    global bet
     total = roll_dice()
     while total != point and total != 7:
         total = roll_dice()
 
     if total == point:
         print("You win!")
-    else:
+        totalwinnings = totalwinnings + bet
+        print("Your current balance is $", totalwinnings)
+    if total == 7:
         print("You lose!")
-
-
+        totalwinnings = totalwinnings - bet
+        if totalwinnings >= 5:
+            print("Your current balance is $", totalwinnings)
+    return totalwinnings
 main()
